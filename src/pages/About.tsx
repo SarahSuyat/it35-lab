40871492cb8ef8cc772a49e78d8951ea0450ad0d
SarahSuyat <suyatsarah519@gmail.com>
2025-02-13 13:39:32 +0800

    Rename Page1 to About

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,7 +9,7 @@ import {
       IonToolbar 
   } from '@ionic/react';
   
-  const Page1: React.FC = () => {
+  const About: React.FC = () => {
     return (
       <IonPage>
         <IonHeader>
@@ -17,7 +17,7 @@ import {
             <IonButtons slot='start'>
               <IonMenuButton></IonMenuButton>
             </IonButtons>
-            <IonTitle>Page1</IonTitle>
+            <IonTitle>About</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen>
@@ -26,4 +26,4 @@ import {
     );
   };
   
-  export default Page1;
+  export default About;
