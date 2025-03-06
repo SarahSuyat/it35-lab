ce5efb2208b9b6ed2ca96aecdbee2b402014dbca
SarahSuyat <suyatsarah519@gmail.com>
2025-03-06 08:40:14 +0800

    Updates

--- assets/swipe-back-legacy-CinqWFNY.js
+++ assets/swipe-back-legacy-CinqWFNY.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CBy0MRsZ.js","./index3-legacy-BfEXnST8.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-TPvbXKb6.js","./index3-legacy-Diz_VMwp.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
