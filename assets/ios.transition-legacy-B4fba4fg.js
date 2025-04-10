318aa21ca509f96eee5fcc6e6454ad3ca08810e3
SarahSuyat <suyatsarah519@gmail.com>
2025-04-10 08:43:35 +0800

    Updates

--- assets/ios.transition-legacy-B4fba4fg.js
+++ assets/ios.transition-legacy-B4fba4fg.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B6-OmDm0.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CoiVrarG.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
