7f5ccea43fbbba4887766b3ea8260f7392495a11
SarahSuyat <suyatsarah519@gmail.com>
2025-04-30 23:30:39 +0800

    Updates

--- assets/ios.transition-legacy-B-IHt-jM.js
+++ assets/ios.transition-legacy-B-IHt-jM.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-o6uZpEyu.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BL4BvfhK.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-vadijkZP.js
+++ assets/swipe-back-legacy-vadijkZP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-o6uZpEyu.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BL4BvfhK.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
